baa9b6a8a8069cf6e2783feb865a61c28e8ac4ae
LTVU <vumadridistazz@gmail.com>
2023-03-01 14:13:19 +0700

    main

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿
+
+$(document).ready(function () {
+    var swiper = new Swiper(".mySwiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+           delay: 5000,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+  
+})
